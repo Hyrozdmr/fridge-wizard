17b882f8236716acc05a6dd4889d67e562cf8cc0
Patrick Skipworth <pskipworth@fastmail.com>
2024-04-24 16:36:36 +0100

    Create fridge in db and fridge model
    Set up url to create fridge and view, set up Fridge object model, works with Postman but no other testing yet

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from pymongo import MongoClient #MongoClient class  from pymongo library need it to connect to mongo DB
+
+# Fridge model handles data structures for fridges, fridge user ownership and stored items with expiry dates
+class Fridge(models.Model):
+
+    id = models.CharField(primary_key=True, max_length=100)
+    storedItems = models.JSONField()
+
+    def save(self, *args, **kwargs): #method to save fridge data into the database
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.id
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,28 +1,57 @@
 # file: views.py
-
+from django.http import HttpResponse
+from pymongo import MongoClient
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from .models import Fridge
 
 # Create your views here.
 
 # Example of importing the database connection logic and creating
 # a database connection
-from .utils import get_db_handle
+# from .utils import get_db_handle
 
-def my_mongo_view():
-    # Your MongoDB connection details
-    host = 'localhost'
-    port = 27017
-    username = 'admin'
-    password = 'admin'
-    db_name = 'fridge_hero'
+# def template_mongo_view():
+#     # Your MongoDB connection details
+#     host = 'localhost'
+#     port = 27017
+#     username = 'admin'
+#     password = 'admin'
+#     db_name = 'fridge_hero'
 
-    # Get the database handle and client
-    db_handle, client = get_db_handle(db_name, host, port, username, password)
+#     # Get the database handle and client
+#     db_handle, client = get_db_handle(db_name, host, port, username, password)
 
-    # Now you can use db_handle to interact with your MongoDB database
-    # For example:
-    collection = db_handle['my_collection']
-    result = collection.find_one({'some_key': 'some_value'})
+#     # Now you can use db_handle to interact with your MongoDB database
+#     # For example:
+#     collection = db_handle['my_collection']
+#     result = collection.find_one({'some_key': 'some_value'})
     
-    # Don't forget to close the client when you're done
+#     # Don't forget to close the client when you're done
+#     client.close()
+
+# fridge_view handles requests to the fridges collection
+@csrf_exempt
+def fridge_view(request, fridge_instance):                 #http request from client
+    # break down the fridge object
+    fridge_id = fridge_instance.id
+    stored_items = fridge_instance.storedItems
+
+    # connect to mongo db
+    client = MongoClient('mongodb://localhost:27017')
+    db = client['fridge_hero_test']
+    collection = db['fridges']
+
+    # testing if i can insert a fridge to the db
+    fridge_data = {
+        'fridge_id' : fridge_id,
+        'items' : stored_items,
+    }
+
+    collection.insert_one(fridge_data)
+    print(fridge_data)
+
+    # Close the client when the request is complete
     client.close()
+
+    return HttpResponse('Fridge created succesfully')

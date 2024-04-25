f7be28988d99d22adadfa6fc260feef8d96a75ad
Samuel <samuel@seedlegals.com>
2024-04-25 15:45:26 +0100

    allows user to add mutliple items to the fridge

--- backend/backend/controllers/fridges.py
+++ backend/backend/controllers/fridges.py
@@ -2,6 +2,10 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from pymongo import MongoClient
 import json
+import pymongo
+from bson import ObjectId
+from pymongo import UpdateOne
+
 
 # Set up function for getting db connection
 def get_db_handle(db_name, host, port, username, password):
@@ -51,6 +55,43 @@ def create(request):
 
     #     # Clean up: close the MongoDB client
     #     client.close()
+
+
+@csrf_exempt
+def add_items(request, fridge_id):
+    if request.method == 'PATCH':
+        try:
+            data = json.loads(request.body)
+            items = data.get('items', [])
+
+            db, client = get_db_handle(db_name='fridge_hero', host='localhost', port=27017, username='', password='')
+            fridges_collection = db['fridges']
+
+            updates = []
+            for item in items:
+                item_category = item.get('category')
+                item_name = item.get('name')
+                expiry_date = item.get('expiry_date')
+                updates.append(
+                    UpdateOne(
+                        {'_id': ObjectId(fridge_id)},
+                        {'$set': {f'storedItems.{item_category}.{item_name}': expiry_date}}
+                    )
+                )
+
+            if updates:
+                update_result = fridges_collection.bulk_write(updates)
+
+            client.close()
+
+            if updates and update_result.modified_count > 0:
+                return JsonResponse({'message': f'{update_result.modified_count} items added successfully'}, status=200)
+            else:
+                return JsonResponse({'message': 'No items were added', 'details': str(update_result.bulk_api_result)}, status=200)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
 
 
 
--- backend/backend/routes/fridges.py
+++ backend/backend/routes/fridges.py
@@ -4,5 +4,5 @@ from backend.controllers import fridges as fridge_controllers
 
 urlpatterns = [
     path('create/', fridge_controllers.create, name='create'),
-    path('<str:fridge_id>/add-item/', fridge_controllers.add_item, name='add_item'),
+    path('<str:fridge_id>/add-items/', fridge_controllers.add_items, name='add_items'),
 ]

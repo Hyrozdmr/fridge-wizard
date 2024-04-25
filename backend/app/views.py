aecb97cad2d2794b5974431c842040ccdffeac2f
Patrick Skipworth <pskipworth@fastmail.com>
2024-04-25 12:26:43 +0100

    Revert file
    Revert views.py file as not updated

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,47 +1,28 @@
 # file: views.py
-from django.http import HttpResponse
-from pymongo import MongoClient
+
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework import viewsets, permissions
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .models import Fridge
-from .serializers import FridgeSerializer
 
-# FridgeViewset handles requests to the fridges collection
-# @csrf_exempt
+# Create your views here.
 
-@api_view(['POST'])
-def create_fridge(request):
-    if request.method == 'POST':
-        # connect to mongo db
-        client = MongoClient('mongodb://localhost:27017')
-        db = client['fridge_hero']
-        collection = db['fridges']
+# Example of importing the database connection logic and creating
+# a database connection
+from .utils import get_db_handle
 
-        serializer = FridgeSerializer(data=request.data)
-        if serializer.is_valid():
-            # Insert data into MongoDB collection
-            collection.insert_one(serializer.validated_data)
-            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+def my_mongo_view():
+    # Your MongoDB connection details
+    host = 'localhost'
+    port = 27017
+    username = 'admin'
+    password = 'admin'
+    db_name = 'fridge_hero'
+
+    # Get the database handle and client
+    db_handle, client = get_db_handle(db_name, host, port, username, password)
+
+    # Now you can use db_handle to interact with your MongoDB database
+    # For example:
+    collection = db_handle['my_collection']
+    result = collection.find_one({'some_key': 'some_value'})
     
-
-# class FridgeViewset(viewsets.ViewSet):
-#     # connect to mongo db
-#     client = MongoClient('mongodb://localhost:27017')
-#     db = client['fridge_hero_test']
-#     collection = db['fridges']
-
-#     permission_classes = [permissions.AllowAny]
-#     serializer_class = FridgeSerializer
-
-#     def create(self, request):
-#         serializer = FridgeSerializer(data=request.data)
-#         if serializer.is_valid():
-#             # Insert data into MongoDB collection
-#             self.collection.insert_one(serializer.validated_data)
-#             return Response(serializer.validated_data)
-#         else:
-#             return Response(serializer.errors, status=400)
+    # Don't forget to close the client when you're done
+    client.close()

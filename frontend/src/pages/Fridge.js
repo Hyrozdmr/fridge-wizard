32fe8793ab31a2089da9b1d12779d50604bc284c
Samuel <samuel@seedlegals.com>
2024-04-29 15:33:27 +0100

    intial commit adds new multiple items form

--- frontend/src/pages/Fridge.js
+++ frontend/src/pages/Fridge.js
@@ -9,16 +9,15 @@ export default function Fridge() {
   const location = useLocation();
   const userId = location.state && location.state.user_id;
   const [currentFridgeContents, setCurrentFridgeContent] = useState([]);
-  const [itemName, setItemName] = useState('');
-  const [itemCategory, setItemCategory] = useState('');
-  const [expiryDate, setExpiryDate] = useState('');
+  const [showForm, setShowForm] = useState(false);
+  const [items, setItems] = useState([]);
   const navigate = useNavigate();
   const categories = [
-    { label: '🥕 Vegetables', value: 'Vegetables' },
-    { label: '🍖 Meat', value: 'Meat' },
-    { label: '🍎 Fruit', value: 'Fruit' },
-    { label: '🧀 Dairy', value: 'Dairy' },
-    { label: '🥫 Miscellaneous', value: 'Misc' }
+    {label: '🥕 Vegetables', value: 'Vegetables'},
+    {label: '🍖 Meat', value: 'Meat'},
+    {label: '🍎 Fruit', value: 'Fruit'},
+    {label: '🧀 Dairy', value: 'Dairy'},
+    {label: '🥫 Miscellaneous', value: 'Misc'}
   ];
 
   useEffect(() => {
@@ -34,84 +33,101 @@ export default function Fridge() {
   // Logic for getting fridge data goes here
   function getFridgeData(data) {
     // Send get request with userData body to get endpoint
-    AxiosInstance.get( 'fridges/get/', { params: { user_id: userId  } })
-    .then(response => {
-      // Handle successful response
-      setCurrentFridgeContent(response.data);
-    })
-    .catch((error) => {
-      // Handle error if POST request fails
-      console.error('Error:', error);
-    });
+    AxiosInstance.get('fridges/get/', {params: {user_id: userId}})
+        .then(response => {
+          // Handle successful response
+          setCurrentFridgeContent(response.data);
+        })
+        .catch((error) => {
+          // Handle error if POST request fails
+          console.error('Error:', error);
+        });
+  }
+
+  function toggleForm() {
+    setShowForm(!showForm);
+    if (!showForm && items.length === 0) {
+      addItem(); // Add first empty item automatically
+    }
+  }
+
+  function handleItemChange(index, field, value) {
+    const newItems = [...items];
+    newItems[index][field] = value;
+    setItems(newItems);
+  }
+
+  function addItem() {
+    const newItem = {name: '', category: '', expiry_date: ''};
+    setItems([...items, newItem]);
   }
 
   function addItemToFridge() {
-    // Check if all fields are filled
-    if (!itemName || !itemCategory || !expiryDate) {
-      alert("Please fill all fields before adding an item.");
-      return;
+    // Validate all items
+    for (const item of items) {
+      if (!item.name || !item.category || !item.expiry_date) {
+        alert("Please fill all fields for all items before adding.");
+        return;
+      }
+      item.expiry_date = new Date(item.expiry_date).toISOString().slice(0, 11) + "00:00:00Z";
     }
 
-    // Format the date to include time ('T00:00:00Z' for midnight UTC)
-    const formattedExpiryDate = new Date(expiryDate).toISOString().slice(0, 11) + "00:00:00Z";
-    console.log("Adding item:", { itemName, itemCategory, formattedExpiryDate });
-    console.log ("fridge contents", currentFridgeContents.fridge_data._id)
-
-    AxiosInstance.patch(`fridges/${currentFridgeContents.fridge_data._id}/add-items/`, {
-      items: [{
-        name: itemName,
-        category: itemCategory,
-        expiry_date: formattedExpiryDate
-      }]
-    })
+    AxiosInstance.patch(`fridges/${currentFridgeContents.fridge_data._id}/add-items/`, {items})
         .then(response => {
-          console.log('Item added:', response.data);
-          // Optionally clear the form here and refresh fridge data
-          setItemName('');
-          setItemCategory('');
-          setExpiryDate('');
+          setItems([]);
+          setShowForm(false);
           getFridgeData(userId);
         })
         .catch(error => {
-          console.error('Error adding item:', error);
+          console.error('Error adding items:', error);
         });
   }
 
   return (
       <div className='fridge-view-container'>
         <div className='open-fridge-image-container'>
-          <button onClick={( navigateToRoot )}>
+          <button onClick={navigateToRoot}>
             <h1>FH</h1>
           </button>
           <div className='open-fridge-image'>
-            <img src={FridgeImage} alt="Fridge" />
+            <img src={FridgeImage} alt="Fridge"/>
           </div>
         </div>
         <div className='item-list'>
-          <h1>What's inside?</h1>
-          <ItemList returnedFridgeData={currentFridgeContents} />
-          <div>
-            <select value={itemCategory} onChange={(e) => setItemCategory(e.target.value)}>
-              {categories.map((option, index) => (
-                  <option key={index} value={option.value}>
-                    {option.label}
-                  </option>
-              ))}
-            </select>
-            <input
-                type="text"
-                placeholder="Item Name"
-                value={itemName}
-                onChange={(e) => setItemName(e.target.value)}
-            />
-            <input
-                type="date"
-                value={expiryDate}
-                onChange={(e) => setExpiryDate(e.target.value)}
-            />
-            <button onClick={addItemToFridge}>Add Item</button>
-          </div>
+          <h1>{showForm ? 'Add Items' : "What's inside?"}</h1>
+          {!showForm && <ItemList returnedFridgeData={currentFridgeContents}/>}
+          {!showForm && <button onClick={toggleForm} className="fridge-form-button">Add Items</button>}
+
+          {showForm && (
+              <>
+                {items.map((item, index) => (
+                    <div key={index}>
+                      <select value={item.category} onChange={e => handleItemChange(index, 'category', e.target.value)}>
+                        {categories.map((option) => (
+                            <option key={option.value} value={option.value}>
+                              {option.label}
+                            </option>
+                        ))}
+                      </select>
+                      <input
+                          type="text"
+                          placeholder="Item Name"
+                          value={item.name}
+                          onChange={e => handleItemChange(index, 'name', e.target.value)}
+                      />
+                      <input
+                          type="date"
+                          value={item.expiry_date}
+                          onChange={e => handleItemChange(index, 'expiry_date', e.target.value)}
+                      />
+                      {index === items.length - 1 && <button onClick={addItem} className="add-more-items-button">+</button>}
+                    </div>
+                ))}
+                <button onClick={() => { addItemToFridge(); toggleForm(); }} className="fridge-form-button">Submit All Items</button>
+                <button onClick={toggleForm} className="fridge-form-button">Cancel</button>
+              </>
+          )}
         </div>
       </div>
-    );
+  );
 }

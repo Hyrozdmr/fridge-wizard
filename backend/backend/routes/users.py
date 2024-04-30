e4d1001161da97f7480a8eecca4ca107ae2bae45
Hyrozdmr <hayriozdemir@hotmail.co.uk>
2024-04-30 10:48:00 +0100

    route was created for get user details function

--- backend/backend/routes/users.py
+++ backend/backend/routes/users.py
@@ -5,4 +5,5 @@ from backend.controllers import users as user_controllers
 urlpatterns = [
     path('signup/', user_controllers.signup, name='user-signup'),
     path('login/', user_controllers.login, name='user-login'),    
+    path('get-user/', user_controllers.get_user, name='get-user'),
 ]

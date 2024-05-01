86effd9f4b848d9a7e8d9f0bbd5a37f27453d317
kevin eboda <kevineboda@gmail.com>
2024-05-01 15:21:38 +0100

    autorization bug fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,7 @@ import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.append('/Users/kevineboda/.local/share/virtualenvs/backend-waKFrOHp/lib/python3.11/site-packages')
+# sys.path.append('/Users/kevineboda/.local/share/virtualenvs/backend-waKFrOHp/lib/python3.11/site-packages')
 
 
 # Quick-start development settings - unsuitable for production
